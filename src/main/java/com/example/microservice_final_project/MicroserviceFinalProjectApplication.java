c61b5e1c2a40104d13ecf457bce8f1b89e2a602d
Dmlehan <dmlehanamanda@gmail.com>
2025-06-25 22:30:48 +0530

    Added readme

---
+++ src/main/java/com/example/microservice_final_project/MicroserviceFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservice_final_project/MicroserviceFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
